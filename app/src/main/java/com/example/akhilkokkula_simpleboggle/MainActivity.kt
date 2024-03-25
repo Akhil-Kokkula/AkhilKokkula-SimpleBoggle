20fd06ec5d39e9f66933be03fcca2ab406dabba3
Akhil Kokkula <kokkulaak@gmail.com>
2024-03-25 16:31:28 -0400

    Added bonus requirement: new game called when device is shaken

--- app/src/main/java/com/example/akhilkokkula_simpleboggle/MainActivity.kt
+++ app/src/main/java/com/example/akhilkokkula_simpleboggle/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.akhilkokkula_simpleboggle
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity(), BoardFragment.BoardFragmentListener, ScoreFragment.ScoreFragmentListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val shakeLimit = 10f
+        var lastShakeTime = 0L
+        val delay = 2000
+        sensorManager.registerListener(object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                    val x = event.values[0]
+                    val y = event.values[1]
+                    val z = event.values[2]
+                    val accel = sqrt((x * x + y * y + z * z).toDouble())
+                    if (accel > shakeLimit && System.currentTimeMillis() - lastShakeTime > delay) {
+                        lastShakeTime = System.currentTimeMillis()
+                        onClickPassed(true)
+                    }
+                }
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+            }
+        }, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onDataPassed(score: Int) {
